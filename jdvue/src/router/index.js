6a765daad80678f678098271d971a1d90c5452e0
夏海峰 <2270900825@qq.com>
2020-12-21 08:58:24 +0800

    前端导航

--- jdvue/src/router/index.js
+++ jdvue/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+/* 商品模块导航 */
+import ShangpinNavigation from '@/components/ShangpinNavigation'
 
 Vue.use(Router)
 
@@ -8,8 +10,11 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'ShangpinNavigation',
+      component: ShangpinNavigation,
+      children:[
+
+      ]
     }
   ]
 })
